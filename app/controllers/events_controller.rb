ed01eafeadd2b3dbb7a4ca8d44f394b771fac470
birdbrother <birdbrother@gmail.com>
2016-10-07 15:48:44 +0800

    event basic crud complete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,42 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+  def new
+    @event = Event.new
+  end
+  def create
+    @event=Event.new(event_params)
+    if @event.save
+      flash[:notice]="新增成功"
+      redirect_to events_path
+    else
+      render :action =>:new
+      flash[:alert]="新增失敗"
+    end
+  end
+  def show
+    @event = Event.find(params[:id])
+  end
+  def edit
+    @event = Event.find(params[:id])
+  end
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      flash[:notice]="修改成功"
+      redirect_to events_path
+    else
+      render :action =>:edit
+      flash[:alert]="修改失敗"
+    end
+  end
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+  private
+  def event_params
+    params.require(:event).permit(:name, :description)
+  end
 end
